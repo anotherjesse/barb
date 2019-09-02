b70ce0ca44bf8b5bdff9dd9a4fcea2e3f6d88f9f
Jesse Andrews <jesse@planet.com>
2019-09-02 09:39:32 -0700

    react + keypress + paperjs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
+import KeyboardEventHandler from 'react-keyboard-event-handler';
 import './App.css';
+import paper from 'paper';
 
-function App() {
+const Paper = props => {
+  const canvas = React.useRef();
+
+  React.useEffect(() => {
+    paper.setup(canvas.current);
+  }, []);
+
+  const line = (x1, y1, x2, y2) => {
+    new paper.Path({strokeColor: 'black', segments: [[x1, y1], [x2, y2]]});
+  };
+
+  const times = n => Array(n).fill();
+  const lerp = (start, stop, amt) => start + (stop - start) * amt;
+
+  const burst = (x, y, r, count = 10) => {
+    times(count)
+      .map(_ => lerp(0, 6.28, Math.random()))
+      .map(theta =>
+        line(x, y, x + r * Math.cos(theta), y + r * Math.sin(theta))
+      );
+  };
+
+  const handleKey = key => {
+    switch (key) {
+      case 'l':
+        return line(
+          lerp(10, 90, Math.random()),
+          lerp(10, 90, Math.random()),
+          lerp(10, 90, Math.random()),
+          lerp(10, 90, Math.random())
+        );
+
+      case 'b':
+        return burst(
+          lerp(10, 90, Math.random()),
+          lerp(10, 90, Math.random()),
+          lerp(10, 20, Math.random())
+        );
+
+      default:
+        console.log('unhandled key', key);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <KeyboardEventHandler
+        handleKeys={['alphanumeric']}
+        onKeyEvent={handleKey}
+      />
+      <canvas ref={canvas} width={400} height={400} />
     </div>
   );
+};
+
+function App() {
+  return <Paper />;
 }
 
 export default App;
